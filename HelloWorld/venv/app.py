478f06395877e93d83c355948aeaed80a3ac4db3
chinnabits <2020mt93203@wilp.bits-pilani.ac.in>
2021-07-11 13:02:42 +0530

    Code for process xl workbook and barchart

--- HelloWorld/venv/app.py
+++
@@ -1,4 +0,0 @@
-from ecommerce.shipping import cal_shipping, cal_tax, track_inventory
-cal_shipping()
-cal_tax()
-track_inventory()
--- HelloWorld/venv/working-with-directories.py
+++
@@ -1,7 +0,0 @@
-# Print files in a directolry
-
-from pathlib import Path
-path = Path()
-for file in path.glob('*'):
-    print(file)
-    
